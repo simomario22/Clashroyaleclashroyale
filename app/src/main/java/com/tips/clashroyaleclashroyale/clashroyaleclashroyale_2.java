32bc1c088fa0748db4f15454e4968812d4c78c5e
simo mariok <simomario22@gmail.com>
2017-08-05 17:13:51 +0100

    with banner programed

--- app/src/main/java/com/tips/clashroyaleclashroyale/clashroyaleclashroyale_2.java
+++ app/src/main/java/com/tips/clashroyaleclashroyale/clashroyaleclashroyale_2.java
@@ -1,16 +1,39 @@
 package com.tips.clashroyaleclashroyale;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class clashroyaleclashroyale_2 extends AppCompatActivity {
 
 
+
+    //  farebase
+    private DatabaseReference mreferance ;
+
     private Button  btn1 ,btn2 , btn3 , btn4 ,btn5 , btn6, btn7,  btn8 ;
 
+    //  url  come from database
+    private String appUrl1 , appUrl2 ,appUrl3 ;
+
+    //  hadi  3la 9bal button bach ya cliki darori
+    private int  a ,b,c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +49,41 @@ public class clashroyaleclashroyale_2 extends AppCompatActivity {
         btn6 = (Button)findViewById(R.id.button2_6);
         btn7 = (Button)findViewById(R.id.button2_7);
         btn8 = (Button)findViewById(R.id.button2_8);
+
+        // ads
+
+        View adContainer = findViewById(R.id.adMobView);
+
+        AdView mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.SMART_BANNER);
+        mAdView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        ((RelativeLayout)adContainer).addView(mAdView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        mAdView.loadAd(adRequest);
+
+
+
+        // Firebase
+
+        mreferance = FirebaseDatabase.getInstance().getReference().child("page2");
+        mreferance.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //  get value from database firebase
+                appUrl1 = dataSnapshot.child("app1").getValue().toString();
+                appUrl2 = dataSnapshot.child("app2").getValue().toString();
+                appUrl3 = dataSnapshot.child("app3").getValue().toString();
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -74,12 +132,27 @@ public class clashroyaleclashroyale_2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(appUrl2));
+                startActivity(intent);
+
+                a = 1;
+
             }
         });
 
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(appUrl1));
+                startActivity(intent);
+                b = 1;
 
             }
         });
@@ -88,14 +161,45 @@ public class clashroyaleclashroyale_2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(appUrl3));
+                startActivity(intent);
+
+                c = 1;
             }
         });
 
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),clashroyaleclashroyale_7.class);
-                startActivity(intent);
+
+                //  chart  hta ycliki 3la buttons b 3
+
+                if (a ==1 &&  b==1  && c== 1 ){
+
+                    Intent intent = new Intent(getApplicationContext(),clashroyaleclashroyale_7.class);
+                    startActivity(intent);
+
+                }else {
+
+                    AlertDialog.Builder builder = new  AlertDialog.Builder(clashroyaleclashroyale_2.this);
+                    builder.setMessage("You Need to Rate the Applications With 5 Stars. it's the IMPORTANT STEP");
+                    builder.setCancelable(true);
+                    builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+
+                        }
+                    });
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+
+
 
             }
         });
--- app/src/main/java/com/tips/clashroyaleclashroyale/clashroyaleclashroyale_7.java
+++ app/src/main/java/com/tips/clashroyaleclashroyale/clashroyaleclashroyale_7.java
@@ -1,13 +1,105 @@
 package com.tips.clashroyaleclashroyale;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class clashroyaleclashroyale_7 extends AppCompatActivity {
+
+    private Button btn1 , btn2 ;
+
+    //  farebase
+    private DatabaseReference mreferance ;
+
+    // url  rate button
+
+    private String rateUrl ;
+
+    private int a ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clashroyaleclashroyale_7);
+
+
+        btn1 = (Button)findViewById(R.id.button7_1);
+        btn2 = (Button )findViewById(R.id.button7_2);
+
+        mreferance = FirebaseDatabase.getInstance().getReference().child("page7");
+        mreferance.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                rateUrl = dataSnapshot.child("rate").getValue().toString();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(rateUrl));
+                startActivity(intent);
+
+                a = 1;
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (a==1){
+
+                    moveTaskToBack(true);
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+
+                }else {
+
+                    AlertDialog.Builder builder = new  AlertDialog.Builder(clashroyaleclashroyale_7.this);
+                    builder.setMessage("This is the FINAL STEP ! You Need to Rate the App With 5 Stars");
+                    builder.setCancelable(true);
+                    builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+
+                        }
+                    });
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+
+
+                }
+            }
+        });
     }
 }
